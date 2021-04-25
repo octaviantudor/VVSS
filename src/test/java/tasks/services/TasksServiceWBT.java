7fb69e6dc69619d9f4d2681a823fe52a108844e0
Pinzariu Octavian-Tudor <octavian.pinzariu@yahoo.com>
2021-04-25 11:47:51 +0300

    LAB03: finished test case implement

--- src/test/java/tasks/services/TasksServiceWBT.java
+++ src/test/java/tasks/services/TasksServiceWBT.java
@@ -10,14 +10,12 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class TasksServiceWBT {
-    private ArrayTaskList tasksECP;
-    private ArrayTaskList tasksBVA;
+
     private TasksService tasksService;
-    private File file = new File("D:\\Facultate Informatica\\Anul III\\Semestrul II\\Verificarea si validarea sistemelor soft\\Laborator\\Tasks\\src\\test\\java\\resources\\test.txt");
 
 
     @Test
-    void filterTasks_BetweenDates_exception_F02_TC01() {
+    void  WBT_INVALID_01() {
         Task task = new Task("Task1", new Date(1000));
         ArrayTaskList arrayTaskList = new ArrayTaskList();
         arrayTaskList.add(task);
@@ -27,8 +25,9 @@ public class TasksServiceWBT {
         });
     }
 
+
     @Test
-    void filterTasks_BetweenDates_exception_F02_TC02() {
+    void  WBT_INVALID_02() {
         Task task = new Task("Task1", new Date(500));
         ArrayTaskList arrayTaskList = new ArrayTaskList();
         arrayTaskList.add(task);
@@ -39,7 +38,7 @@ public class TasksServiceWBT {
     }
 
     @Test
-    void filterTasks_BetweenDates_found_F02_TC03() {
+    void WBT_VALID() {
         Task task = new Task("Task1", new Date(1500));
         task.setActive(true);
         ArrayTaskList arrayTaskList = new ArrayTaskList();
@@ -55,40 +54,5 @@ public class TasksServiceWBT {
         }
     }
 
-    @Test
-    void filterTasks_BetweenDates_found_F02_TC04() {
-        Task task = new Task("Task1", new Date(2000));
-        task.setActive(true);
-        ArrayTaskList arrayTaskList = new ArrayTaskList();
-        arrayTaskList.add(task);
-        tasksService = new TasksService(arrayTaskList);
 
-        try {
-            ArrayList<Task> taskIterable = (ArrayList<Task>) tasksService.filterTasks(new Date(1000), new Date(2000));
-            assert (taskIterable.size() == 1);
-        }
-        catch (Exception e) {
-            assert(false);
-        }
-    }
-
-    @Test
-    void filterTasks_BetweenDates_found_F02_TC05() {
-        Task task1 = new Task("Task1", new Date(500));
-        task1.setActive(true);
-        Task task2 = new Task("Task1", new Date(2000));
-        task2.setActive(true);
-        ArrayTaskList arrayTaskList = new ArrayTaskList();
-        arrayTaskList.add(task1);
-        arrayTaskList.add(task2);
-        tasksService = new TasksService(arrayTaskList);
-
-        try {
-            ArrayList<Task> taskIterable = (ArrayList<Task>) tasksService.filterTasks(new Date(1000), new Date(2000));
-            assert (taskIterable.size() == 1);
-        }
-        catch (Exception e) {
-            assert(false);
-        }
-    }
 }
